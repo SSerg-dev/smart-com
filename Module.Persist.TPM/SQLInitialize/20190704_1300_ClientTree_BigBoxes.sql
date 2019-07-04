40c9c10959145bbcb5800209b7df80e4881d062a
Егор Нам <egor.nam@smartcom.software>
2019-07-04 11:47:41 +0000

    (RC) Task 117104: REVISION (Client - Client hierarchy) - Отсутствует доработка bigboxes
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C36749

--- Module.Persist.TPM/SQLInitialize/20190704_1300_ClientTree_BigBoxes.sql
+++ Module.Persist.TPM/SQLInitialize/20190704_1300_ClientTree_BigBoxes.sql
@@ -1,3 +1,1 @@
-﻿UPDATE NodeType SET Priority = 3 WHERE Priority = 2;
-INSERT INTO NodeType (Disabled, Type, Name, Priority) VALUES(0, 'CLIENT', 'Group Chain', 2);
-
+﻿INSERT INTO NodeType (Disabled, Type, Name, Priority) VALUES(0, 'CLIENT', 'Group Chain', 2);
