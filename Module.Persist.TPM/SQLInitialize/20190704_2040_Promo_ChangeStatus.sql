97b710fc727b0173f07c8329f7672dbea7b3dd7b
Андрей Филюшкин <andrey.filyushkin@smartcom.software>
2019-07-05 06:38:27 +0000

    (RC) Правка скрипта
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C36773

--- Module.Persist.TPM/SQLInitialize/20190704_2040_Promo_ChangeStatus.sql
+++ Module.Persist.TPM/SQLInitialize/20190704_2040_Promo_ChangeStatus.sql
@@ -1,3 +1,3 @@
 ﻿UPDATE [dbo].[Promo]
-SET [PromoStatusId] = (SELECT [Id] FROM [dbo].[PromoStatus] WHERE [SystemName] = 'Deleted')
+SET [PromoStatusId] = (SELECT [Id] FROM [dbo].[PromoStatus] WHERE [SystemName] = 'Deleted' AND [Disabled] = 0)
 WHERE ([Disabled] = 1)
