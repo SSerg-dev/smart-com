b7bfc24add976646bb0d5370056a29d951b6a6c8
Андрей Филюшкин <andrey.filyushkin@smartcom.software>
2019-12-19 11:40:19 +0000

    (RC) Докоммит: 1)правка версии Newtonsoft, 2)точки доступа
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C39557

---
+++ Module.Persist.TPM/SQLInitialize/20191219_1427_Historical_Delete_UserRole_AP.sql
@@ -0,0 +1,18 @@
+﻿-- точки доступа
+-- избавляемся от дублей
+DELETE FROM AccessPointRole WHERE AccessPointId IN (SELECT Id FROM ACCESSPOINT WHERE [Resource] IN ('HistoricalUserRoles', 'DeletedUserRoles') AND [Action] IN ('GetHistoricalUserRoles', 'GetDeletedUserRoles'));
+DELETE FROM AccessPoint WHERE [Resource] IN ('HistoricalUserRoles', 'DeletedUserRoles') AND [Action] IN ('GetHistoricalUserRoles', 'GetDeletedUserRoles') AND [Disabled] = 'false';
+
+INSERT INTO AccessPoint (Resource, Action, Disabled, DeletedDate) VALUES ('HistoricalUserRoles', 'GetHistoricalUserRoles', 0, NULL);
+INSERT INTO AccessPoint (Resource, Action, Disabled, DeletedDate) VALUES ('DeletedUserRoles', 'GetDeletedUserRoles', 0, NULL);
+
+-- роли
+--Administrator
+--Historical 
+INSERT INTO AccessPointRole
+(Id, RoleId, AccessPointId)
+SELECT NEWID(), (SELECT ID FROM [Role] Where SystemName = 'Administrator' AND [Disabled] = 'false'), Id FROM ACCESSPOINT WHERE [Resource]='HistoricalUserRoles' AND [Action] IN ('GetHistoricalUserRoles');
+--Deleted 
+INSERT INTO AccessPointRole
+(Id, RoleId, AccessPointId)
+SELECT NEWID(), (SELECT ID FROM [Role] Where SystemName = 'Administrator' AND [Disabled] = 'false'), Id FROM ACCESSPOINT WHERE [Resource]='DeletedUserRoles' AND [Action] IN ('GetDeletedUserRoles');
