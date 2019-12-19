dfac14a86df5d583529bfe07dc16ca6fb8cab166
alexander.zinkevich <alexander.zinkevich@smartcom.tfs.local>
2019-12-19 11:36:40 +0000

    (RC) (Promo) - Некорректное название Промо с механикой VP из TLC
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C39556

---
+++ Module.Persist.TPM/Sql/RenamePromoVP.sql
@@ -0,0 +1,8 @@
+UPDATE [dbo].[Promo]
+	SET  
+	  [Name] = CONCAT(SUBSTRING (Name, 0 ,CHARINDEX('VP ',Name)),'VP',' ',(SELECT Name FROM MechanicType WHERE Id = MarsMechanicTypeId))
+	
+	WHERE Name LIKE '%VP%|%' ESCAPE '|'   
+GO
+
+
