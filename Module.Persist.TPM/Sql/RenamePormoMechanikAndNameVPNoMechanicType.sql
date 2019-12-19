f368777b3f96123044974b1619f99a05a1a9f3b3
alexander.zinkevich <alexander.zinkevich@smartcom.tfs.local>
2019-12-19 13:11:24 +0000

    Скрипт на изменение названия и механик промо для VP
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C39560

---
+++ Module.Persist.TPM/Sql/RenamePormoMechanikAndNameVPNoMechanicType.sql
@@ -0,0 +1,8 @@
+UPDATE [dbo].[Promo]
+   SET  
+       [Name] = CONCAT(SUBSTRING (Name, 0 ,CHARINDEX('VP ',Name)),'TPR',' ',MarsMechanicDiscount,'%')
+      ,[MarsMechanicId] = (SELECT Id FROM Mechanic  WHERE Name = 'TPR' AND PromoTypesId = [dbo].[Promo].[PromoTypesId]) 
+      ,[Mechanic] = CONCAT('TPR ', MarsMechanicDiscount,'%')  
+   WHERE Mechanic LIKE('VP%')  AND Mechanic NOT LIKE('%+%')
+
+
