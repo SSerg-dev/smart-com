a236da760657d9bf7d70c572dfa331cec3296f6a
Егор Нам <egor.nam@smartcom.software>
2020-01-22 10:22:15 +0000

    (RC) Task 157819: REVISION CMM: (Promo - Basic) - Не загружается форма Select Product *
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C39891

--- Module.Persist.TPM/SQLInitialize/20200122_1300_Promo_GetSelectedProducts_CMM.sql
+++ Module.Persist.TPM/SQLInitialize/20200122_1300_Promo_GetSelectedProducts_CMM.sql
@@ -1,4 +1,14 @@
-﻿-- SuperReader
-INSERT INTO AccessPointRole
-(Id, RoleId, AccessPointId)
-SELECT NEWID(), (SELECT ID FROM [Role] Where SystemName = 'CMManager' AND [Disabled] = 'false'), Id FROM ACCESSPOINT WHERE [Resource]='Products' AND [Action]='GetSelectedProducts' AND [Disabled] = 'False'
+﻿BEGIN TRANSACTION;
+
+DECLARE @role_name VARCHAR(255) = 'CMManager';
+DECLARE @resource_name VARCHAR(255) = 'Products';
+DECLARE @action_name VARCHAR(255) = 'GetSelectedProducts';
+
+DELETE AccessPointRole 
+WHERE RoleId = (SELECT ID FROM [Role] Where SystemName = @role_name AND [Disabled] = 'false') AND 
+	AccessPointId = (SELECT Id FROM ACCESSPOINT WHERE [Resource]= @resource_name AND [Action]= @action_name AND [Disabled] = 'false');
+
+INSERT INTO AccessPointRole (Id, RoleId, AccessPointId) 
+	VALUES(NEWID(), (SELECT ID FROM [Role] Where SystemName = @role_name AND [Disabled] = 'false'), (SELECT Id FROM ACCESSPOINT WHERE [Resource]= @resource_name AND [Action]= @action_name AND [Disabled] = 'false'));
+
+COMMIT;
