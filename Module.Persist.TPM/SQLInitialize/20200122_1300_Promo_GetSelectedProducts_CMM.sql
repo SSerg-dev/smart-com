27d58bca14b6c8684ec589ecc1cc5aecbcfec6e6
Егор Нам <egor.nam@smartcom.software>
2020-01-22 10:00:50 +0000

    (RC) Task 157819: REVISION CMM: (Promo - Basic) - Не загружается форма Select Product *
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C39889

---
+++ Module.Persist.TPM/SQLInitialize/20200122_1300_Promo_GetSelectedProducts_CMM.sql
@@ -0,0 +1,4 @@
+﻿-- SuperReader
+INSERT INTO AccessPointRole
+(Id, RoleId, AccessPointId)
+SELECT NEWID(), (SELECT ID FROM [Role] Where SystemName = 'CMManager' AND [Disabled] = 'false'), Id FROM ACCESSPOINT WHERE [Resource]='Products' AND [Action]='GetSelectedProducts' AND [Disabled] = 'False'
