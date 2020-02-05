62fda56a993656d417c87f1a5cf66ed04eae7977
Егор Нам <egor.nam@smartcom.software>
2020-02-05 08:16:24 +0000

    (RC) CIP 957: правка скриптов
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C40117

--- Module.Persist.TPM/SQLInitialize/20200203_1700_PromoROIReportNotificationAndHandler.sql
+++ Module.Persist.TPM/SQLInitialize/20200203_1700_PromoROIReportNotificationAndHandler.sql
@@ -1,9 +1,13 @@
 ﻿DECLARE @notificationName VARCHAR(255) = 'PROMO_ROI_REPORT_NOTIFICATION';
 DECLARE @handlerName VARCHAR(255) = 'Module.Host.TPM.Handlers.Notifications.PromoROIReportNotificationHandler';
+DECLARE @promoRoiReportMarsDayFullNameWithoutYear VARCHAR(255) = 'PROMO_ROI_REPORT_MARS_DAY_FULL_NAME_WITHOUT_YEAR'
+DECLARE @promoRoiReportMarsWeekName VARCHAR(255) = 'PROMO_ROI_REPORT_MARS_WEEK_NAME'
 
 DELETE [dbo].[Recipient]  WHERE [MailNotificationSettingId] = (SELECT TOP(1) Id FROM [dbo].[MailNotificationSetting] WHERE [name] = @notificationname AND [Disabled] = 0);
 DELETE [dbo].[MailNotificationSetting]  WHERE [Name] = @notificationName;
 DELETE [dbo].[LoopHandler]  WHERE [Name] = @handlerName;
+DELETE [dbo].[LoopHandler]  WHERE [Name] = @promoRoiReportMarsDayFullNameWithoutYear;
+DELETE [dbo].[LoopHandler]  WHERE [Name] = @promoRoiReportMarsWeekName;
 
 INSERT INTO [dbo].[MailNotificationSetting] ([Id] ,[Name] ,[Description] ,[Subject] ,[Body] ,[IsDisabled] ,[To] ,[CC] ,[BCC] ,[Disabled] ,[DeletedDate])
      VALUES (NEWID() ,@notificationName ,'Promo ROI Report notification.' ,'Promo ROI Report' ,'/' ,0 ,'' ,NULL ,NULL ,0 ,NULL);
@@ -12,7 +16,7 @@ INSERT INTO [dbo].[LoopHandler] ([Id] ,[Description] ,[Name] ,[ExecutionPeriod] 
     VALUES (NEWID() ,N'Sending notification promo ROI report' ,@handlerName ,86400000 ,'SCHEDULE' ,GETDATE() ,'2020-02-05 08:00:00.0000000 +03:00' ,'2020-02-06 08:00:00.0000000 +03:00' ,'PROCESSING' ,NULL ,NULL ,NULL ,NULL);
 
 INSERT INTO [dbo].[Setting] ([Id], [Name], [Type], [Value], [Description])
-     VALUES (NEWID(), 'PROMO_ROI_REPORT_PERIOD_NAME', 'string', 'P5', 'After this date, a letter for the previous year will not be sent.');
+     VALUES (NEWID(), @promoRoiReportMarsDayFullNameWithoutYear, 'string', 'P5 W1 D1', 'After this date, a letter for the previous year will not be sent.');
 
 INSERT INTO [dbo].[Setting] ([Id], [Name], [Type], [Value], [Description])
-     VALUES (NEWID(), 'PROMO_ROI_REPORT_MARS_WEEK_NAME', 'string', 'W4', 'Mars week name.');
+     VALUES (NEWID(), @promoRoiReportMarsWeekName, 'string', 'W4', 'Mars week name.');
