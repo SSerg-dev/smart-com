5dde261c2fc3c5e9b134e2c69f5b8be5ab664d3c
Егор Нам <egor.nam@smartcom.software>
2020-02-12 12:24:18 +0000

    (RC) Task 161085: REVISION (Finance - Actual COGS/TI) - Некорректные данные в истории при редактировании
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C40248

--- Module.Persist.TPM/Model/History/HistoricalCOGS.cs
+++ Module.Persist.TPM/Model/History/HistoricalCOGS.cs
@@ -1,20 +1,20 @@
+using System;
 using Core.History;
 using Module.Persist.TPM.Model.TPM;
-using System;
 
 namespace Module.Persist.TPM.Model.History
 {
     [AssociatedWith(typeof(COGS))]
-    public class HistoricalCOGS : BaseHistoricalEntity<System.Guid>
+    public class HistoricalCOGS : BaseHistoricalEntity<Guid>
     {
-        public int ClientTreeId { get; set; }
-        public System.Guid? BrandTechId { get; set; }
+        public int? ClientTreeId { get; set; }
+        public Guid? BrandTechId { get; set; }
         public DateTimeOffset? StartDate { get; set; }
         public DateTimeOffset? EndDate { get; set; }
-        public float LSVpercent { get; set; }
+        public float? LSVpercent { get; set; }
 
         public string BrandTechName { get; set; }
         public string ClientTreeFullPathName { get; set; }
-        public int ClientTreeObjectId { get; set; }
+        public int? ClientTreeObjectId { get; set; }
     }
 }
