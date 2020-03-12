526a437e6932c9b08077c895ae6cfccb84f7a27a
alexander.zinkevich <alexander.zinkevich@smartcom.tfs.local>
2020-03-12 15:13:09 +0000

    (RC) REVISION (Product - Promo Product Correction) - Отсутствует импортируемое значение в результатах импорта
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C40767

--- Module.Persist.TPM/Model/Import/ImportPromoProductsUplift.cs
+++ Module.Persist.TPM/Model/Import/ImportPromoProductsUplift.cs
@@ -12,9 +12,11 @@ namespace Module.Persist.TPM.Model.Import
     public class ImportPromoProductsUplift : BaseImportEntity
     {
         [ImportCSVColumn(ColumnNumber = 0)]
+        [Display(Name = "ZREP")]
         public string ZREP { get; set; }
 
         [ImportCSVColumn(ColumnNumber = 1)]
+        [Display(Name = "Plan Product Uplift, %")]
         public string PlanProductUpliftPercent { get; set; }
     }
 }
