8e4d2ff1d97abf0292ba75ca0be2310e920ded03
Андрей Вологдин <andrey.vologdin@smartcom.software>
2020-03-31 15:54:45 +0000

    (ReleaseCandidate) Некорректный набор операций под ролью Super Reader
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C41074

---
+++ Module.Persist.TPM/SQLInitialize/20200331_1740_Alter_NonPromoSupportBrandTech_AP.sql
@@ -0,0 +1,5 @@
+﻿--SuperReader
+DELETE FROM AccessPointRole 
+WHERE 
+	RoleId = (SELECT ID FROM [Role] WHERE SystemName = 'SuperReader' AND [Disabled] = 'false')
+	AND AccessPointId IN (SELECT Id FROM ACCESSPOINT WHERE [Resource] = 'NonPromoSupportBrandTeches' AND [Action] IN ('Put', 'Post', 'Delete', 'ModifyNonPromoSupportBrandTechList'));
