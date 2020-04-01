cd6e6315ce3ee10c62fa56a8a0fa497f44ff6e1d
Андрей Филюшкин <andrey.filyushkin@smartcom.software>
2020-04-01 10:09:50 +0000

    (RC) Откат коммита 41105
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C41112

--- Module.Frontend.TPM/Client/view/tpm/costproduction/CostProduction.js
+++ Module.Frontend.TPM/Client/view/tpm/costproduction/CostProduction.js
@@ -37,7 +37,71 @@
     ],
 
     dockedItems: [{
-        xtype: 'custombigtoolbar',
+        xtype: 'readonlydirectorytoolbar',
+        items: [{
+            xtype: 'widthexpandbutton',
+            ui: 'fill-gray-button-toolbar',
+            text: l10n.ns('core', 'selectablePanelButtons').value('toolbarCollapse'),
+            glyph: 0xf13d,
+            glyph1: 0xf13e,
+            target: function () {
+                return this.up('toolbar');
+            },
+        }, {
+            itemId: 'updatebutton',
+            action: 'PatchCostProduction',
+            glyph: 0xf64f,
+            text: l10n.ns('core', 'crud').value('updateButtonText'),
+            tooltip: l10n.ns('core', 'crud').value('updateButtonText')
+        }, '-', {
+            itemId: 'updategroupbutton',
+            glyph: 0xf4f0,
+            action: 'PatchCostProduction',
+            text: l10n.ns('tpm', 'button').value('updateGroupButtonText'),
+            tooltip: l10n.ns('tpm', 'button').value('updateGroupButtonText'),
+        }, {
+            itemId: 'extfilterbutton',
+            glyph: 0xf349,
+            text: l10n.ns('core', 'toptoolbar').value('filterButtonText'),
+            tooltip: l10n.ns('core', 'toptoolbar').value('filterButtonText')
+        }, {
+            itemId: 'detailfilter',
+            glyph: 0xf233,
+            text: l10n.ns('tpm', 'filter', 'buttons').value('PromoSupportDetailFilter'),
+            tooltip: l10n.ns('tpm', 'filter', 'buttons').value('PromoSupportDetailFilter')
+        }, {
+            itemId: 'deletedbutton',
+            resource: 'Deleted{0}',
+            action: 'Get{0}',
+            glyph: 0xf258,
+            text: l10n.ns('core', 'toptoolbar').value('deletedButtonText'),
+            tooltip: l10n.ns('core', 'toptoolbar').value('deletedButtonText')
+        }, {
+            itemId: 'historybutton',
+            resource: 'Historical{0}',
+            action: 'Get{0}',
+            glyph: 0xf2da,
+            text: l10n.ns('core', 'crud').value('historyButtonText'),
+            tooltip: l10n.ns('core', 'crud').value('historyButtonText')
+        }, {
+            itemId: 'deletedbutton',
+            resource: 'Deleted{0}',
+            action: 'Get{0}',
+            glyph: 0xf258,
+            text: l10n.ns('core', 'toptoolbar').value('deletedButtonText'),
+            tooltip: l10n.ns('core', 'toptoolbar').value('deletedButtonText')
+        }, '-', '->', '-', {
+            itemId: 'extfilterclearbutton',
+            ui: 'blue-button-toolbar',
+            disabled: true,
+            glyph: 0xf232,
+            text: l10n.ns('core', 'filter').value('filterEmptyStatus'),
+            tooltip: l10n.ns('core', 'filter').value('filterEmptyStatus'),
+            overCls: '',
+            style: {
+                'cursor': 'default'
+            }
+        }],
         dock: 'right'
     }],
 
