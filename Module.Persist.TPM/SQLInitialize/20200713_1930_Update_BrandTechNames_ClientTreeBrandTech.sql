47e8b1f62eaab1fcf0a5d1fa72811fe516cbabbc
Андрей Вологдин <andrey.vologdin@smartcom.software>
2020-07-13 16:40:48 +0000

    (RC) Обновил имена BrabdTech в Client Share
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C42756

---
+++ Module.Persist.TPM/SQLInitialize/20200713_1930_Update_BrandTechNames_ClientTreeBrandTech.sql
@@ -0,0 +1,24 @@
+﻿DECLARE [BrandTechCursor] CURSOR FAST_FORWARD
+	FOR SELECT bt.[Id], bt.[BrandsegTechsub] FROM [dbo].[BrandTech] bt;
+
+DECLARE 
+	@BTID UNIQUEIDENTIFIER,
+	@BTName NVARCHAR(MAX);
+
+OPEN [BrandTechCursor];
+WHILE 1 = 1
+BEGIN
+	FETCH NEXT
+		FROM [BrandTechCursor]
+		INTO 		
+			@BTID,
+			@BTName;
+			
+	IF (SELECT FETCH_STATUS FROM SYS.DM_EXEC_CURSORS(0) WHERE NAME = 'BrandTechCursor') <> 0
+		BREAK;
+
+	UPDATE [dbo].[ClientTreeBrandTech] SET [CurrentBrandTechName] = @BTName WHERE [BrandTechId] = @BTID;
+END;
+
+CLOSE [BrandTechCursor];
+DEALLOCATE [BrandTechCursor];
