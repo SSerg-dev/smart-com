3283a740d38e66cb9f0834a21f757356c50e2117
Алексей Кондратьев <alexey.kondratiev@smartcom.software>
2020-07-14 10:36:30 +0000

    (RC) - Правка скрипта 20200713_1930_Update_BrandTechNames_ClientTreeBrandTech.sql
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C42776

--- Module.Persist.TPM/SQLInitialize/20200713_1930_Update_BrandTechNames_ClientTreeBrandTech.sql
+++ Module.Persist.TPM/SQLInitialize/20200713_1930_Update_BrandTechNames_ClientTreeBrandTech.sql
@@ -1,24 +1,5 @@
-﻿DECLARE [BrandTechCursor] CURSOR FAST_FORWARD
-	FOR SELECT bt.[Id], bt.[BrandsegTechsub] FROM [dbo].[BrandTech] bt;
-
-DECLARE 
-	@BTID UNIQUEIDENTIFIER,
-	@BTName NVARCHAR(MAX);
-
-OPEN [BrandTechCursor];
-WHILE 1 = 1
-BEGIN
-	FETCH NEXT
-		FROM [BrandTechCursor]
-		INTO 		
-			@BTID,
-			@BTName;
-			
-	IF (SELECT FETCH_STATUS FROM SYS.DM_EXEC_CURSORS(0) WHERE NAME = 'BrandTechCursor') <> 0
-		BREAK;
-
-	UPDATE [dbo].[ClientTreeBrandTech] SET [CurrentBrandTechName] = @BTName WHERE [BrandTechId] = @BTID;
-END;
-
-CLOSE [BrandTechCursor];
-DEALLOCATE [BrandTechCursor];
+﻿UPDATE [dbo].[ClientTreeBrandTech]
+   SET 
+      [CurrentBrandTechName] = (Select BrandTech.BrandsegTechsub from BrandTech where BrandTech.Id = BrandTechId)
+	WHERE [CurrentBrandTechName] != (Select BrandTech.BrandsegTechsub from BrandTech where BrandTech.Id = BrandTechId)
+GO
