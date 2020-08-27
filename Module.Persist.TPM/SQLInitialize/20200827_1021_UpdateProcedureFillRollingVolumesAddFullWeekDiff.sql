a0266a05a87dd9309425b2ae53acc5d1559928f3
alexander.zinkevich <alexander.zinkevich@smartcom.tfs.local>
2020-08-27 07:28:51 +0000

    (RC)Внести изменения в таблицу ROLLING_VOLUMES_FDM и процедуру FillRollingVolumes из-за добавления поля FullWeekDiff
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C43497

---
+++ Module.Persist.TPM/SQLInitialize/20200827_1021_UpdateProcedureFillRollingVolumesAddFullWeekDiff.sql
@@ -0,0 +1,53 @@
+CREATE OR ALTER PROCEDURE [dbo].[FillRollingVolumes] AS
+  BEGIN
+	TRUNCATE TABLE [dbo].[RollingVolume];
+
+    INSERT INTO [dbo].[RollingVolume]
+           ([Id]
+           ,[DemandGroup]
+           ,[Week]
+           ,[PlanProductIncrementalQTY]
+           ,[Actuals]
+           ,[OpenOrders]
+           ,[Baseline]
+           ,[ActualIncremental]
+           ,[PreviousRollingVolumes]
+           ,[Lock]
+           ,[ProductId]
+           ,[DMDGroup]
+           ,[PromoDifference]
+           ,[ActualOO]
+           ,[PreliminaryRollingVolumes]
+           ,[RollingVolumesTotal]
+           ,[ManualRollingTotalVolumes]
+		   ,[FullWeekDiff])
+		SELECT 
+			NEWID(), 
+			CONCAT(rvf.DMDGROUP, '_05_0125'),
+			CAST(d.MarsYear AS NVARCHAR(4)) + 'P' + REPLACE(STR(d.MarsPeriod, 2), SPACE(1), '0') + d.MarsWeekName,
+			rvf.PlanProductIncrementalQty, 
+			rvf.ActualsQty, 
+			rvf.OpenOrdersQty, 
+			rvf.BaselineQty, 
+			rvf.ActualIncrementalQty,
+			rvf.PreviousRollingVolumesQty, 
+			0, 
+			p.Id, 
+			ct.DMDGroup, 
+			rvf.PromoDifferenceQty,
+			rvf.ActualOpenOrdersQty,
+			rvf.PreliminaryRollingVolumesQty,
+			rvf.RollingVolumesQty,
+			rvf.RollingVolumesQty,
+			rvf.FullWeekDiff
+
+		FROM [dbo].[ROLLING_VOLUMES_FDM] AS rvf
+		JOIN [dbo].[Product] AS p ON p.ZREP = rvf.ZREP
+		JOIN [dbo].[Dates] AS d ON d.OriginalDate = DATEADD(day, -7, rvf.WeekStartDate)
+		JOIN [dbo].[ClientTree] AS ct ON (ct.DemandCode = CONCAT(rvf.DMDGROUP, '_05_0125')
+											AND ct.EndDate IS NULL)
+   END
+
+
+
+
---
+++ Module.Persist.TPM/SQLInitialize/20200827_1022_UpdateRollingVolumesFDM_Add_FullWeekDiff.sql
@@ -0,0 +1,21 @@
+
+IF EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME = 'ROLLING_VOLUMES_FDM' AND XTYPE = 'U')        
+DROP TABLE [ROLLING_VOLUMES_FDM]                                                              
+                IF NOT EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME = 'ROLLING_VOLUMES_FDM' AND XTYPE = 'U')   
+                   CREATE TABLE [dbo].[ROLLING_VOLUMES_FDM](				                                    
+                   	[ZREP] [nvarchar](100) NULL,				                                            
+                   	[DMDGROUP] [nvarchar](max) NULL,				                                        
+                   	[WeekStartDate] [datetime] NULL,				                                        
+                   	[PlanProductIncrementalQty] [float] NULL,			                                    
+                   	[ActualsQty] [float] NULL,				                                                
+                   	[OpenOrdersQty] [float] NULL,				                                            
+                   	[ActualOpenOrdersQty] [float] NULL,				                                        
+                   	[BaselineQty] [float] NULL,				                                                
+                   	[ActualIncrementalQty] [float] NULL,				                                    
+                   	[PreliminaryRollingVolumesQty] [float] NULL,			                                
+                   	[PreviousRollingVolumesQty] [float] NULL,			                                    
+                   	[PromoDifferenceQty] [float] NULL,             		                                    
+                   	[RollingVolumesQty] [float] NULL,	
+					[FullWeekDiff] [float] NULL
+                   ) ON [PRIMARY]                           
+
