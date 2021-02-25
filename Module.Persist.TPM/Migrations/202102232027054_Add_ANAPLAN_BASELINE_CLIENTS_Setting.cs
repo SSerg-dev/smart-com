e65292a9ca290fca11323e8c69b9b1b3afb0c0e8
Андрей Филюшкин <andrey.filyushkin@smartcom.software>
2021-02-25 10:28:29 +0000

    (RC) Add ANAPLAN_BASELINE_CLIENTS migration
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C45585

---
+++ Module.Persist.TPM/Migrations/202102232027054_Add_ANAPLAN_BASELINE_CLIENTS_Setting.cs
@@ -0,0 +1,56 @@
+namespace Module.Persist.TPM.Migrations
+{
+    using Core.Settings;
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_ANAPLAN_BASELINE_CLIENTS_Setting : DbMigration
+    {
+        public override void Up()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            if (defaultSchema.Equals("Jupiter"))
+            {
+                Sql($@"
+                    {AddSetting}
+                    go
+                ");
+            }
+        }
+        
+        public override void Down()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            if (defaultSchema.Equals("Jupiter"))
+            {
+                Sql($@"
+                    {DeleteSetting}
+                    go
+                ");
+            }
+        }
+
+        private string AddSetting =
+        @"
+            if not exists(select * from [Jupiter].[Setting] where [Name] = 'ANAPLAN_BASELINE_CLIENTS')
+                insert into [Jupiter].[Setting]
+                           ([Id]
+                           ,[Name]
+                           ,[Type]
+                           ,[Value]
+                           ,[Description])
+                     values
+                           (newid()
+                           ,'ANAPLAN_BASELINE_CLIENTS'
+                           ,'String'
+                           ,''
+                           ,'Anaplan g-hierarchy codes')
+        ";
+
+        private string DeleteSetting =
+        @"
+            if exists(select * from [Jupiter].[Setting] where [Name] = 'ANAPLAN_BASELINE_CLIENTS')
+                delete from [Jupiter].[Setting] where [Name] = 'ANAPLAN_BASELINE_CLIENTS'
+        ";
+    }
+}
