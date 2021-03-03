1d92bf3661a907f26ff5449e31ac971a15143992
Илья Черноскутов <ilya.chernoskutov@smartcom.software>
2021-03-03 14:08:45 +0000

    (RC) Изменение уровня валидации при синхронизации клиентов.
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C45647

--- Module.Host.TPM/Actions/DataLakeIntegrationActions/MarsCustomersCheckAction.cs
+++ Module.Host.TPM/Actions/DataLakeIntegrationActions/MarsCustomersCheckAction.cs
@@ -40,19 +40,7 @@ namespace Module.Host.TPM.Actions
                         List<Tuple<string, string>> incidentValues = new List<Tuple<string, string>>();
                         List<string> addedNodes = new List<string>();
 
-                        var uniqueValues = marsCustomers.Where(n => n.ZCUSTHG02 != "0" && n.ZCUSTHG02 != null).Select(x => x.ZCUSTHG02).Distinct().ToList();
-                        foreach (var value in uniqueValues)
-                        {
-                            pairList.Add(new Tuple<string, string>("ZCUSTHG02", value));
-                        }
-
-                        uniqueValues = marsCustomers.Where(n => n.ZCUSTHG03 != "0" && n.ZCUSTHG03 != null).Select(x => x.ZCUSTHG03).Distinct().ToList();
-                        foreach (var value in uniqueValues)
-                        {
-                            pairList.Add(new Tuple<string, string>("ZCUSTHG03", value));
-                        }
-
-                        uniqueValues = marsCustomers.Where(n => n.ZCUSTHG04 != "0" && n.ZCUSTHG04 != null).Select(x => x.ZCUSTHG04).Distinct().ToList();
+                        var uniqueValues = marsCustomers.Where(n => n.ZCUSTHG04 != "0" && n.ZCUSTHG04 != null).Select(x => x.ZCUSTHG04).Distinct().ToList();
                         foreach (var value in uniqueValues)
                         {
                             pairList.Add(new Tuple<string, string>("ZCUSTHG04", value));
@@ -61,8 +49,6 @@ namespace Module.Host.TPM.Actions
                         var clientTreeNodes = context.Set<ClientTree>().Where(x => !x.EndDate.HasValue);
                         var clientGhierarchyCodeList = clientTreeNodes.Select(x => x.GHierarchyCode).ToList();
 
-                        // упорядочиваем, чтобы проверка начиналась с младших узлов
-                        pairList = pairList.OrderByDescending(x => x.Item1).ToList();
 
                         foreach (var pair in pairList)
                         {
@@ -71,23 +57,9 @@ namespace Module.Host.TPM.Actions
                                 if (!addedNodes.Contains(pair.Item2))
                                 {
                                     incidentValues.Add(pair);
-
-                                    // добавили узел = попала вся ветка (чтобы не было дубликатов в нотификации)
-                                    if (pair.Item1 == "ZCUSTHG02")
-                                    {
-                                        addedNodes.Add(pair.Item2);
-                                    }
-                                    else if (pair.Item1 == "ZCUSTHG03")
-                                    {
-                                        var rec = marsCustomers.Where(x => x.ZCUSTHG03 == pair.Item2).FirstOrDefault();
-                                        addedNodes.Add(rec.ZCUSTHG02);
-                                        addedNodes.Add(rec.ZCUSTHG03);
-                                    }
-                                    else if (pair.Item1 == "ZCUSTHG04")
+                                    if (pair.Item1 == "ZCUSTHG04")
                                     {
                                         var rec = marsCustomers.Where(x => x.ZCUSTHG04 == pair.Item2).FirstOrDefault();
-                                        addedNodes.Add(rec.ZCUSTHG02);
-                                        addedNodes.Add(rec.ZCUSTHG03);
                                         addedNodes.Add(rec.ZCUSTHG04);
                                     }
                                 }
