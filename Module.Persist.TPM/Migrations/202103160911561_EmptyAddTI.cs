3e837cb1f352bdc93dc43b215992ed0f0adb884c
Алексей Кондратьев <alexey.kondratiev@smartcom.software>
2021-03-16 09:14:16 +0000

    (AddTI) Пустая миграция
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C45781

---
+++ Module.Persist.TPM/Migrations/202103160911561_EmptyAddTI.cs
@@ -0,0 +1,16 @@
+namespace Module.Persist.TPM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmptyAddTI : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
