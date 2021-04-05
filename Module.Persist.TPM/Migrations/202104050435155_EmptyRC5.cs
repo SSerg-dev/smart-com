0a6999c9107b87e3bf60fcb10f7106baed89ced5
Алексей Кондратьев <alexey.kondratiev@smartcom.software>
2021-04-05 04:39:59 +0000

    (RC) Пустая миграция
    git-tfs-id: [https://tfs.smartcom.software/tfs/DotNET_Project_Collection]$/TPM/ReleaseCandidate/Source;C46021

---
+++ Module.Persist.TPM/Migrations/202104050435155_EmptyRC5.cs
@@ -0,0 +1,16 @@
+namespace Module.Persist.TPM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmptyRC5 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
