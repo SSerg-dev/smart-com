275cc6d101084fee2c2acfd775759f43a873277d
karen.kaplanyan <karen.kaplanyan@smartcom.software>
2021-09-30 15:35:09 +0300

    RPA update

--- Module.Frontend.TPM/Controllers/RPAsController.cs
+++ Module.Frontend.TPM/Controllers/RPAsController.cs
@@ -124,7 +124,7 @@ namespace Module.Frontend.TPM.Controllers
 
 					result.Constraint = String.Join(";", this.constraints.Select(x => x.Value).ToArray());
 					result.CreateDate = DateTime.UtcNow;
-					result.FileURL = $"<a href=https://tpmuiuxsa.blob.core.windows.net/jupiteruiuxcontainer/RPAFiles/{Path.GetFileName(fileName)} download>Download file</a>";                    
+					result.FileURL = $"<a href=https://marsanalyticsshareddevsa.blob.core.windows.net/russiapetcarejupiter/RPAFiles/{Path.GetFileName(fileName)} download>Download file</a>";                    
 					var resultSaveChanges = Context.SaveChanges();
 
 
