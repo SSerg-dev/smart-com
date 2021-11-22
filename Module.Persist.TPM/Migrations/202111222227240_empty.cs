80028d9573e65cc85b1c2ab75764fb2d3a601fb6
Илья Черноскутов <ilya.chernoskutov@smartcom.software>
2021-11-23 05:28:42 +0700

    (RPA_Actuals) empty migration

---
+++ Module.Persist.TPM/Migrations/202111222227240_empty.cs
@@ -0,0 +1,17 @@
+namespace Module.Persist.TPM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class empty : DbMigration
+    {
+        public override void Up()
+        {
+          
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
