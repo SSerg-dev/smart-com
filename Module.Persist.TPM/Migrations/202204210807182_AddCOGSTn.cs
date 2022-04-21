2c14f2e0fc439a08a6a84a65abe8d4de1649660b
Kiril <kiril.peralta@smartcom.software>
2022-04-21 11:14:27 +0300

    [304108] Add migration COGS/Tn

---
+++ Module.Persist.TPM/Migrations/202204210807182_AddCOGSTn.cs
@@ -0,0 +1,75 @@
+namespace Module.Persist.TPM.Migrations
+{
+    using Core.Settings;
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCOGSTn : DbMigration
+    {
+        public override void Up()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            CreateTable(
+                $"{defaultSchema}.PlanCOGSTn",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false, identity: true),
+                        Volume = c.Int(nullable: false),
+                        Disabled = c.Boolean(nullable: false),
+                        DeletedDate = c.DateTimeOffset(precision: 7),
+                        ClientTreeId = c.Int(nullable: false),
+                        BrandTechId = c.Guid(),
+                        StartDate = c.DateTimeOffset(precision: 7),
+                        EndDate = c.DateTimeOffset(precision: 7),
+                        LSVpercent = c.Single(nullable: false),
+                        Year = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey($"{defaultSchema}.BrandTech", t => t.BrandTechId)
+                .ForeignKey($"{defaultSchema}.ClientTree", t => t.ClientTreeId)
+                .Index(t => t.Id)
+                .Index(t => t.ClientTreeId)
+                .Index(t => t.BrandTechId);
+            
+            CreateTable(
+                $"{defaultSchema}.ActualCOGSTn",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false, identity: true),
+                        Volume = c.Int(nullable: false),
+                        Disabled = c.Boolean(nullable: false),
+                        DeletedDate = c.DateTimeOffset(precision: 7),
+                        ClientTreeId = c.Int(nullable: false),
+                        BrandTechId = c.Guid(),
+                        StartDate = c.DateTimeOffset(precision: 7),
+                        EndDate = c.DateTimeOffset(precision: 7),
+                        LSVpercent = c.Single(nullable: false),
+                        Year = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey($"{defaultSchema}.BrandTech", t => t.BrandTechId)
+                .ForeignKey($"{defaultSchema}.ClientTree", t => t.ClientTreeId)
+                .Index(t => t.Id)
+                .Index(t => t.ClientTreeId)
+                .Index(t => t.BrandTechId);
+            
+        }
+        
+        public override void Down()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            DropForeignKey($"{defaultSchema}.ActualCOGSTn", "ClientTreeId", $"{defaultSchema}.ClientTree");
+            DropForeignKey($"{defaultSchema}.ActualCOGSTn", "BrandTechId", $"{defaultSchema}.BrandTech");
+            DropForeignKey($"{defaultSchema}.PlanCOGSTn", "ClientTreeId", $"{defaultSchema}.ClientTree");
+            DropForeignKey($"{defaultSchema}.PlanCOGSTn", "BrandTechId", $"{defaultSchema}.BrandTech");
+            DropIndex($"{defaultSchema}.ActualCOGSTn", new[] { "BrandTechId" });
+            DropIndex($"{defaultSchema}.ActualCOGSTn", new[] { "ClientTreeId" });
+            DropIndex($"{defaultSchema}.ActualCOGSTn", new[] { "Id" });
+            DropIndex($"{defaultSchema}.PlanCOGSTn", new[] { "BrandTechId" });
+            DropIndex($"{defaultSchema}.PlanCOGSTn", new[] { "ClientTreeId" });
+            DropIndex($"{defaultSchema}.PlanCOGSTn", new[] { "Id" });
+            DropTable($"{defaultSchema}.ActualCOGSTn");
+            DropTable($"{defaultSchema}.PlanCOGSTn");
+        }
+    }
+}
