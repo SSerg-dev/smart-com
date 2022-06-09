2042855fb708df943bd0114f33f86e3b184dfc02
Kiril <kiril.peralta@smartcom.software>
2022-06-09 15:32:06 +0300

    [MovingAzure] Add empty migration

---
+++ Module.Persist.TPM/Migrations/202206091227525_Add_Empty_Migration.cs
@@ -0,0 +1,15 @@
+namespace Module.Persist.TPM.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_Empty_Migration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
