5d6d5a633bbaa73b1b2cc65432ee84f5d82af0b3
Риф Яруллин <rif.iarullin@smartcom.software>
2022-07-11 17:17:43 +0500

    (RSmode) добавлен переключатель режимов CSS

--- Frontend.TPM/Client/app/view/core/main/TopToolbar.js
+++ Frontend.TPM/Client/app/view/core/main/TopToolbar.js
@@ -4,38 +4,49 @@
     ui: 'marengo-toolbar',
     padding: '0 9 0 9',
 
-    items: [{
-        xtype: 'expandbutton',
-        ui: 'white-button-toolbar',
-        glyph: 0xf35c,
-        glyph1: 0xf06d,
-        target: '#drawer'
-    }, {
-        xtype: 'tbtext',
-        ui: 'menu-path',
-        itemId: 'breadcrumbs'
-    }, '->', {
-        xtype: 'securitybutton',
-        cls: 'rolebutton',
-        glyph: 0xf019,
-        itemId: 'rolebutton',
-        text: l10n.ns('core').value('defaultRoleButtonText')
-    }, {
-        xtype: 'securitybutton',
-        cls: 'userbutton',
-        glyph: 0xf004,
-        itemId: 'userbutton'
-    }, {
-        xtype: 'button',
-        ui: 'light-blue-button-toolbar',
-        //hidden: true,
-        glyph: 0xf206,
-        itemId: 'exitbutton',
-        tooltip: l10n.ns('core').value('exitButtonText'),
-        disabled: true
-    }, {
-        xtype: 'tbspacer',
-        width: 5
-    }]
-
+    items: [
+        {
+            xtype: 'expandbutton',
+            ui: 'white-button-toolbar',
+            glyph: 0xf35c,
+            glyph1: 0xf06d,
+            target: '#drawer'
+        },
+        {
+            xtype: 'tbtext',
+            ui: 'menu-path',
+            itemId: 'breadcrumbs'
+        },
+        '->',
+        {
+            xtype: 'modelswitcher',
+        },
+        '->',
+        {
+            xtype: 'securitybutton',
+            cls: 'rolebutton',
+            glyph: 0xf019,
+            itemId: 'rolebutton',
+            text: l10n.ns('core').value('defaultRoleButtonText')
+        },
+        {
+            xtype: 'securitybutton',
+            cls: 'userbutton',
+            glyph: 0xf004,
+            itemId: 'userbutton'
+        },
+        {
+            xtype: 'button',
+            ui: 'light-blue-button-toolbar',
+            //hidden: true,
+            glyph: 0xf206,
+            itemId: 'exitbutton',
+            tooltip: l10n.ns('core').value('exitButtonText'),
+            disabled: true
+        },
+        {
+            xtype: 'tbspacer',
+            width: 5
+        }
+    ]
 });
---
+++ Frontend.TPM/Client/app/view/core/modeswitcher/ModeSwitcher.js
@@ -0,0 +1,7 @@
+﻿Ext.define('App.view.core.modelswitcher.ModeSwitcher', {
+    extend: 'Ext.Component',
+    alias: 'widget.modelswitcher',
+    autoEl: {
+        html: '<input type="checkbox" id="modelswitchercheckbox" class="modelswitchercheckbox" name="topping" >Rolling Scenario mode'
+    },
+});
