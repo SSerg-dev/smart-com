5fdb517b4110c6de9128648bde20cec71044679e
Илья Черноскутов <ilya.chernoskutov@smartcom.software>
2022-08-04 03:10:34 +0700

    (RSmode) добавление mode в incremental promo

--- Module.Persist.TPM/Model/TPM/IncrementalPromo.cs
+++ Module.Persist.TPM/Model/TPM/IncrementalPromo.cs
@@ -1,16 +1,18 @@
 ﻿using Core.Data;
+using Module.Persist.TPM.Model.Interfaces;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Module.Persist.TPM.Model.TPM
 {
-    public class IncrementalPromo : IEntity<Guid>, IDeactivatable
+    public class IncrementalPromo : IEntity<Guid>, IDeactivatable, IMode
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public System.Guid Id { get; set; }
         public bool Disabled { get; set; }
         [Index("Unique_IncrementalPromo", 1, IsUnique = true)]
         public DateTimeOffset? DeletedDate { get; set; }
+        public TPMmode TPMmode { get; set; }
 
         public double? PlanPromoIncrementalCases { get; set; }
 
