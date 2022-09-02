671cb9ab7c82ad3e356d20eee2e0a7fb3d235aa2
Pavel Sergeev <pavel.sergeev@smartcom.software>
2022-09-02 11:00:31 +0300

    Added Dev1 confgis

---
+++ Scripts/ReplaceConfigs.ps1
@@ -0,0 +1,58 @@
+﻿
+#requires -version 4
+<#
+.SYNOPSIS
+	SFM3 deployment script
+  
+.NOTES
+	Version:        1.0
+  	Author:         Pavel Sergeev, Smartcom LLC
+  	Creation Date:  02.09.2022
+  	Change Date:    02.09.2022
+
+#>
+
+#---------------------------------------------------------[ Script Parameters ]------------------------------------------------------
+
+param (
+	[Parameter(Mandatory)]
+	[string] $SourcesDirectory,
+	[Parameter(Mandatory)]
+	[string] $Environment
+)
+
+#---------------------------------------------------------[ Initialization ]--------------------------------------------------------
+
+$ErrorActionPreference = "Stop"
+
+$Configurations = @(
+	'Frontend.TPM\Web.config',
+	'ProcessingService.TPM\App.config',
+	'Module.Persist.TPM\app.config'
+)
+
+#-----------------------------------------------------------[ Execution ]------------------------------------------------------------
+
+try {
+	Write-Host "Preparing configuration files"
+	foreach ($Configuration in $Configurations) {
+		$ConfigFile = $Configuration
+		$ConfigEnvFile = $Configuration + '.' + $Environment
+		$ConfigPath = Join-Path -Path $SourcesDirectory -ChildPath $Configuration
+		$ConfigEnvPath = Join-Path -Path $SourcesDirectory -ChildPath ($Configuration + '.' + $Environment)
+
+		Write-Host "Replace config file '$ConfigEnvFile' to '$ConfigFile'"
+		Copy-Item -Path $ConfigEnvPath -Destination $ConfigPath -Force
+	}
+
+	Write-Host "Done"
+} catch {
+	Write-Host $($_.Exception.Message)
+	throw $($_.Exception.Message)
+}
+	
+	
+
+
+
+
