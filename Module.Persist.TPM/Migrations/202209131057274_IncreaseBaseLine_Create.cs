c35ba578cb6270caeccae0f0a3f95c49d33c3ac2
karen.kaplanyan <karen.kaplanyan@smartcom.software>
2022-09-14 08:57:14 +0300

    (PriceIncrease) Создание новой таблицы инкризного бейзлайна

---
+++ Module.Persist.TPM/Migrations/202209131057274_IncreaseBaseLine_Create.cs
@@ -0,0 +1,42 @@
+namespace Module.Persist.TPM.Migrations
+{
+    using Core.Settings;
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IncreaseBaseLine_Create : DbMigration
+    {
+        public override void Up()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            CreateTable(
+                 $"{defaultSchema}.IncreaseBaseLine",
+                 c => new
+                 {
+                     Id = c.Guid(nullable: false, identity: true),
+                     Disabled = c.Boolean(nullable: false),
+                     DeletedDate = c.DateTimeOffset(precision: 7),
+                     ProductId = c.Guid(nullable: false),
+                     DemandCode = c.String(nullable: false, maxLength: 255),
+                     StartDate = c.DateTimeOffset(precision: 7),
+                     InputBaseLineQty = c.Double(nullable: false),
+                     SellInBaseLineQty = c.Double(nullable: false),
+                     SellOutBaseLineQty = c.Double(nullable: false),
+                     Type = c.Int(nullable: false),
+                 })
+                 .PrimaryKey(t => t.Id)
+                 .Index(t => new { t.DemandCode, t.StartDate, t.DeletedDate, t.ProductId }, unique: true, name: "Unique_IncreaseBaseLine");
+
+                  AddForeignKey($"{defaultSchema}.IncreaseBaseLine", "ProductId", $"{defaultSchema}.Product", "Id");
+        }
+        
+        public override void Down()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            DropForeignKey($"{defaultSchema}.IncreaseBaseLine", "ProductId", $"{defaultSchema}.Product");
+            DropIndex($"{defaultSchema}.IncreaseBaseLine", "Unique_IncreaseBaseLine");
+            DropTable($"{ defaultSchema}.IncreaseBaseLine");
+        }
+        
+    }
+}
