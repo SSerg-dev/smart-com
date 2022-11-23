492aaabcd3d315aee60b0f7f3192fa96a220fe3c
AlekseyLoktev83 <alekseyloktev83@gmail.com>
2022-11-23 16:56:01 +0300

    Airflow. Увеличил предельноу время копирование таблицы до 30 минут. 	modified:   JupiterYandex/airflow/dags/jupiter_calculation/promo_calculation/jupiter_calc_copy.py

--- JupiterYandex/airflow/dags/jupiter_calculation/promo_calculation/jupiter_calc_copy.py
+++ JupiterYandex/airflow/dags/jupiter_calculation/promo_calculation/jupiter_calc_copy.py
@@ -302,7 +302,7 @@ with DAG(
     start_mon_detail = start_monitoring_detail(dst_dir=parameters["MaintenancePathPrefix"], upload_path=parameters["UploadPath"], runid=parameters["RunId"], entities=generate_upload_script(
         start_mon, parameters["MaintenancePathPrefix"], RAW_SCHEMA_FILE, parameters["UploadPath"], parameters["BcpParameters"], parameters["CurrentUploadDate"], parameters["LastUploadDate"]))
 # Upload entities from sql to hdfs in parallel
-    upload_tables = BashOperator.partial(task_id="upload_tables", do_xcom_push=True,execution_timeout=datetime.timedelta(minutes=20), retries=2).expand(
+    upload_tables = BashOperator.partial(task_id="upload_tables", do_xcom_push=True,execution_timeout=datetime.timedelta(minutes=30), retries=2).expand(
         bash_command=generate_bcp_script(
             upload_path=parameters["UploadPath"], bcp_parameters=parameters["BcpParameters"], entities=start_mon_detail),
     )
