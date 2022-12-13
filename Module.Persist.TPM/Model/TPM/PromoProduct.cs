ac13ab2dfcbad661bc9e78698006fbfcbe5a6094
Риф Яруллин <rif.iarullin@smartcom.software>
2022-12-13 12:27:54 +0500

    (BugFixes) fix plu virtual in promoproduct

--- Module.Persist.TPM/Model/TPM/PromoProduct.cs
+++ Module.Persist.TPM/Model/TPM/PromoProduct.cs
@@ -241,7 +241,7 @@ namespace Module.Persist.TPM.Model.TPM
         public Guid ProductId { get; set; }
         public virtual Product Product { get; set; }
 
-        public PromoProduct2Plu Plu { get; set; } //view
+        public virtual PromoProduct2Plu Plu { get; set; } //view
 
         public ICollection<PromoProductsCorrection> PromoProductsCorrections { get; set; }
 
