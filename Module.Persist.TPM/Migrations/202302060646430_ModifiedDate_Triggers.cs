14d9d020c3d50384949e36be5fda4d99e3d0be33
Илья Черноскутов <ilya.chernoskutov@smartcom.software>
2023-02-07 16:53:07 +0300

    (develop) fix multiple triggers

--- Module.Persist.TPM/Migrations/202302060646430_ModifiedDate_Triggers.cs
+++ Module.Persist.TPM/Migrations/202302060646430_ModifiedDate_Triggers.cs
@@ -14,11 +14,8 @@ namespace Module.Persist.TPM.Migrations
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "Brand"));
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "BrandTech"));
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "ClientTreeBrandTech"));
-            Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "ClientTree"));
-            Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "AssortmentMatrix"));
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "Product"));
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "Mechanic"));
-            Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "NoneNego"));
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "MechanicType"));
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "ProductTree"));
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "Technology"));
@@ -87,10 +84,44 @@ namespace Module.Persist.TPM.Migrations
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "RPASetting"));
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "RPA"));
             Sql(ViewMigrations.UpdateTriggerModifiedDate(defaultSchema, "MetricsLiveHistories"));
+            SqlString = SqlString.Replace("DefaultSchemaSetting", defaultSchema);
+            Sql(SqlString);
         }
 
         public override void Down()
         {
         }
+
+        private string SqlString = @"
+        
+            ALTER TRIGGER [DefaultSchemaSetting].[NoneNego_Update_Trigger]
+            ON [DefaultSchemaSetting].[NoneNego]
+            AFTER INSERT, UPDATE AS
+              UPDATE [DefaultSchemaSetting].[NoneNego]
+              SET ModifiedDate = CURRENT_TIMESTAMP,[ToDate] = '9999-12-28T23:59:59.999+00:00'
+              WHERE Id IN (SELECT DISTINCT Id FROM Inserted)
+            GO
+
+            ALTER TRIGGER [DefaultSchemaSetting].[AssortmentMatrix_ChangesIncident_Insert_Update_Trigger]
+	            ON [DefaultSchemaSetting].[AssortmentMatrix]
+	            AFTER INSERT, UPDATE
+	            AS
+		            INSERT INTO ChangesIncident ([DirectoryName], [ItemId], [CreateDate], [ProcessDate], [DeletedDate], [Disabled])
+		            SELECT 'AssortmentMatrix', INSERTED.Id, GETDATE(), NULL, NULL, 0 FROM INSERTED
+                    UPDATE [DefaultSchemaSetting].[AssortmentMatrix]
+                    SET ModifiedDate = CURRENT_TIMESTAMP
+                    WHERE Id IN (SELECT DISTINCT Id FROM Inserted)
+            GO
+
+            ALTER TRIGGER [DefaultSchemaSetting].[ClientTreeDemandCode_Update_Trigger]
+            ON [DefaultSchemaSetting].[ClientTree]
+            AFTER INSERT, UPDATE AS
+              UPDATE [DefaultSchemaSetting].ClientTree
+              SET DemandCode = NULL
+              WHERE Id IN (SELECT DISTINCT Id FROM Inserted) and DemandCode = ''
+              UPDATE [DefaultSchemaSetting].[ClientTree]
+              SET ModifiedDate = CURRENT_TIMESTAMP
+              WHERE Id IN (SELECT DISTINCT Id FROM Inserted)
+        ";
     }
 }
