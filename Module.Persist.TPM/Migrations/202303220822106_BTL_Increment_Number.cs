7a12421329f0a99fb30b25e4eda6b10362b5d964
Риф Яруллин <rif.iarullin@smartcom.software>
2023-03-22 13:56:58 +0500

    (fix) BTL incremental and unlink btlpromo

---
+++ Module.Persist.TPM/Migrations/202303220822106_BTL_Increment_Number.cs
@@ -0,0 +1,32 @@
+﻿namespace Module.Persist.TPM.Migrations
+{
+    using Core.Settings;
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class BTL_Increment_Number : DbMigration
+    {
+        public override void Up()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            SqlString = SqlString.Replace("DefaultSchemaSetting", defaultSchema);
+            Sql(SqlString);
+        }
+        
+        public override void Down()
+        {
+        }
+        private string SqlString = @" 
+            ALTER TRIGGER [DefaultSchemaSetting].[BTL_Increment_Number] ON [DefaultSchemaSetting].[BTL] AFTER INSERT AS
+
+            BEGIN
+	            UPDATE b
+		            SET 
+			            b.Number = (SELECT ISNULL((SELECT MAX(Number) FROM [DefaultSchemaSetting].[BTL] WHERE Number < 999999), 0) + 1)
+		            FROM
+			            [DefaultSchemaSetting].[BTL] b
+			            INNER JOIN Inserted i ON i.Id = b.Id
+            END;
+            ";
+    }
+}
