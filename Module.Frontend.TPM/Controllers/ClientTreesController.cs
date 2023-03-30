c86e07ea85df0576c1745df8febf862d4b158d52
Риф Яруллин <rif.iarullin@smartcom.software>
2023-03-30 17:53:17 +0500

    (develop) fix client hierarchy

--- Module.Frontend.TPM/Controllers/ClientTreesController.cs
+++ Module.Frontend.TPM/Controllers/ClientTreesController.cs
@@ -657,6 +657,7 @@ namespace Module.Frontend.TPM.Controllers
         }
 
         [ClaimsAuthorize]
+        [EnableQuery(MaxNodeCount = int.MaxValue, MaxExpansionDepth = 3)]
         public IHttpActionResult UpdateNode([FromBody] ClientTree model)
         {
             if (!ModelState.IsValid)
