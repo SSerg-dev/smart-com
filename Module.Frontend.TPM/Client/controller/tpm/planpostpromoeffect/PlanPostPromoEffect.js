a0acf3483ea8c9697228ddf25f770764a53a376c
anzhul <andrey.zhulidov@waveaccess.ru>
2023-04-18 07:49:12 +0300

    (PPE) Add Historical and Deleted controllers. Access Points to them

---
+++ Module.Frontend.TPM/Client/controller/tpm/planpostpromoeffect/PlanPostPromoEffect.js
@@ -0,0 +1,93 @@
+Ext.define('App.controller.tpm.planpostpromoeffect.PlanPostPromoEffect', {
+    extend: 'App.controller.core.AssociatedDirectory',
+    mixins: ['App.controller.core.ImportExportLogic'],
+
+    init: function () {
+        this.listen({
+            component: {
+                'planpostpromoeffect[isSearch!=true] directorygrid': {
+                    load: this.onGridStoreLoad,
+                    itemdblclick: this.onDetailButtonClick
+                },
+                'planpostpromoeffect directorygrid': {
+                    selectionchange: this.onGridSelectionChange,
+                    afterrender: this.onGridAfterrender,
+                    extfilterchange: this.onExtFilterChange
+                },
+                'planpostpromoeffect #datatable': {
+                    activate: this.onActivateCard
+                },
+                'planpostpromoeffect #detailform': {
+                    activate: this.onActivateCard
+                },
+                'planpostpromoeffect #detailform #prev': {
+                    click: this.onPrevButtonClick
+                },
+                'planpostpromoeffect #detailform #next': {
+                    click: this.onNextButtonClick
+                },
+                'planpostpromoeffect #detail': {
+                    click: this.switchToDetailForm
+                },
+                'planpostpromoeffect #table': {
+                    click: this.onTableButtonClick
+                },
+                'planpostpromoeffect #extfilterbutton': {
+                    click: this.onFilterButtonClick
+                },
+                'planpostpromoeffect #deletedbutton': {
+                    click: this.onDeletedButtonClick
+                },
+                'planpostpromoeffect #createbutton': {
+                    click: this.onCreateButtonClick
+                },
+                'planpostpromoeffect #updatebutton': {
+                    click: this.onUpdateButtonClick
+                },
+                'planpostpromoeffect #deletebutton': {
+                    click: this.onDeleteButtonClick
+                },
+                'planpostpromoeffect #historybutton': {
+                    click: this.onHistoryButtonClick
+                },
+                'planpostpromoeffect #refresh': {
+                    click: this.onRefreshButtonClick
+                },
+                'planpostpromoeffect #close': {
+                    click: this.onCloseButtonClick
+                },
+                // import/export
+                'planpostpromoeffect #exportbutton': {
+                    click: this.onExportButtonClick
+                },
+                'planpostpromoeffect #loadimportbutton': {
+                    click: this.onShowImportFormButtonClick
+                },
+                'planpostpromoeffect #loadimporttemplatebutton': {
+                    click: this.onLoadImportTemplateButtonClick
+                },
+                'planpostpromoeffect #applyimportbutton': {
+                    click: this.onApplyImportButtonClick
+                }
+            }
+        });
+    },
+
+    onHistoryButtonClick: function (button) {
+        var grid = this.getGridByButton(button),
+            selModel = grid.getSelectionModel();
+
+        if (selModel.hasSelection()) {
+            var panel = grid.up('combineddirectorypanel'),
+                model = panel.getBaseModel(),
+                viewClassName = App.Util.buildViewClassName(panel, model, 'Historical');
+
+            var baseReviewWindow = Ext.widget('basereviewwindow', { items: Ext.create(viewClassName, { baseModel: model }) });
+            baseReviewWindow.show();
+
+            var store = baseReviewWindow.down('grid').getStore();
+            var proxy = store.getProxy();
+            proxy.extraParams.Id = this.getRecordId(selModel.getSelection()[0]);
+        }
+    }
+});
