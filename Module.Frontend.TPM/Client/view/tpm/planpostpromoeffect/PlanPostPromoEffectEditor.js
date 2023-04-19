eff9c5f4ba5f61e33b6c042c21e58f699ee220cc
anzhul <andrey.zhulidov@waveaccess.ru>
2023-04-19 14:33:27 +0300

    (PPE) Fix Update of controllers

--- Module.Frontend.TPM/Client/view/tpm/planpostpromoeffect/PlanPostPromoEffectEditor.js
+++ Module.Frontend.TPM/Client/view/tpm/planpostpromoeffect/PlanPostPromoEffectEditor.js
@@ -10,138 +10,144 @@ Ext.define('App.view.tpm.planpostpromoeffect.PlanPostPromoEffectEditor', {
         xtype: 'editorform',
         columnsCount: 1,
         items: [{
-            xtype: 'treesearchfield',
-            name: 'ClientTreeId',
-            fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('ClientTreeFullPathName'),
-            selectorWidget: 'clienttree',
-            valueField: 'Id',
-            displayField: 'FullPathName',
-            clientTreeIdValid: true,
-            store: {
-                storeId: 'clienttreestore',
-                model: 'App.model.tpm.clienttree.ClientTree',
-                autoLoad: false,
-                root: {}
-            },
-            listeners:
-                {
-                    change: function (field, newValue, oldValue) {
-                        if (field && field.record && field.record.data.ObjectId === 5000000) {
-                            this.clientTreeIdValid = false;
-                        } else {
-                            this.clientTreeIdValid = true;
+                xtype: 'treesearchfield',
+                name: 'ClientTreeId',
+                fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('ClientTreeFullPathName'),
+                selectorWidget: 'clienttree',
+                valueField: 'Id',
+                displayField: 'FullPathName',
+                clientTreeIdValid: true,
+                store: {
+                    storeId: 'clienttreestore',
+                    model: 'App.model.tpm.clienttree.ClientTree',
+                    autoLoad: false,
+                    root: {}
+                },
+                listeners:
+                    {
+                        change: function (field, newValue, oldValue) {
+                            if (field && field.record && field.record.data.ObjectId === 5000000) {
+                                this.clientTreeIdValid = false;
+                            } else {
+                                this.clientTreeIdValid = true;
+                            }
                         }
+                    },
+                validator: function () {
+                    if (!this.clientTreeIdValid) {
+                        return l10n.ns('core', 'customValidators').value('clientTreeSelectRoot')
+                    }
+                    return true;
+                },
+                mapping: [{
+                    from: 'FullPathName',
+                    to: 'ClientTreeFullPathName'
+                }]
+            },
+            {
+                xtype: 'searchfield',
+                fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('BrandTechName'),
+                name: 'BrandTechId',
+                selectorWidget: 'brandtech',
+                allowBlank: true,
+                allowOnlyWhitespace: true,
+                valueField: 'Id',
+                displayField: 'BrandsegTechsub',
+                onTrigger2Click: function () {
+                    var technology = this.up().down('[name=BrandTechId]');
+    
+                    this.clearValue();
+                    technology.setValue(null);
+                },
+                listeners: {
+                    afterrender: function (field) {
+                        if (!field.value) {
+                            field.value = null;
+                        }
+                    },
+                    change: function (field, newValue, oldValue) {
+                        var brandtech = field.up().down('[name=BrandTechId]');
+                        var brandtechValue = newValue ? field.record.get('BrandsegTechsub') : null;
+    
+                        brandtech.setValue(brandtechValue);
                     }
                 },
-            validator: function () {
-                if (!this.clientTreeIdValid) {
-                    return l10n.ns('core', 'customValidators').value('clientTreeSelectRoot')
-                }
-                return true;
-            },
-            mapping: [{
-                from: 'FullPathName',
-                to: 'ClientTreeFullPathName'
-            }]
-        },
-        {
-            xtype: 'searchfield',
-            fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('BrandTechName'),
-            name: 'BrandTechId',
-            selectorWidget: 'brandtech',
-            allowBlank: true,
-            allowOnlyWhitespace: true,
-            valueField: 'Id',
-            displayField: 'BrandsegTechsub',
-            onTrigger2Click: function () {
-                var technology = this.up().down('[name=BrandTechId]');
-
-                this.clearValue();
-                technology.setValue(null);
-            },
-            listeners: {
-                afterrender: function (field) {
-                    if (!field.value) {
-                        field.value = null;
+                store: {
+                    type: 'directorystore',
+                    model: 'App.model.tpm.brandtech.BrandTech',
+                    extendedFilter: {
+                        xclass: 'App.ExtFilterContext',
+                        supportedModels: [{
+                            xclass: 'App.ExtSelectionFilterModel',
+                            model: 'App.model.tpm.brandtech.BrandTech',
+                            modelId: 'efselectionmodel'
+                        }]
                     }
                 },
-                change: function (field, newValue, oldValue) {
-                    var brandtech = field.up().down('[name=BrandTechId]');
-                    var brandtechValue = newValue ? field.record.get('BrandsegTechsub') : null;
-
-                    brandtech.setValue(brandtechValue);
-                }
-            },
-            store: {
-                type: 'directorystore',
-                model: 'App.model.tpm.brandtech.BrandTech',
+                mapping: [{
+                    from: 'BrandsegTechsub',
+                    to: 'BrandTechName'
+                }]
+            }, {
+                xtype: 'textfield',
+                name: 'Size',
+                fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('Size')
+            }, {
+                xtype: 'searchfield',
+                editable: false,
+                name: 'DiscountRangeId',
+                fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('DiscountRangeName'),
+                selectorWidget: 'discountrange',
+                valueField: 'Id',
+                displayField: 'Name',
+                entityType: 'DiscountRange',
+                allowBlank: false,
+                allowOnlyWhitespace: false,
+                store: {
+                    type: 'simplestore',
+                    autoLoad: false,
+                    model: 'App.model.tpm.discountrange.DiscountRange'
+                },
                 extendedFilter: {
                     xclass: 'App.ExtFilterContext',
                     supportedModels: [{
                         xclass: 'App.ExtSelectionFilterModel',
-                        model: 'App.model.tpm.brandtech.BrandTech',
+                        model: 'App.model.tpm.discountrange.DiscountRange',
                         modelId: 'efselectionmodel'
                     }]
-                }
-            },
-            mapping: [{
-                from: 'BrandsegTechsub',
-                to: 'BrandTechName'
-            }]
-        }, {
-                xtype: 'textfield',
-                dataIndex: 'Size',
-                fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('Size')
+                },
+                mapping: [{
+                    from: 'Name',
+                    to: 'DiscountRangeName'
+                }]
             }, {
-                xtype: 'combobox',
+                xtype: 'searchfield',
                 editable: false,
-                dataIndex: 'DiscountRangeId',
-                displayField: 'Name',
-                entityType: 'DiscountRange',
-                fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('DiscountRangeName'),
-                queryMode: 'local',
+                name: 'DurationRangeId',
+                fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('DurationRangeName'),
+                selectorWidget: 'durationrange',
                 valueField: 'Id',
-                forceSelection: true,
-                allowBlank: false,
-                allowOnlyWhitespace: false,
-                store: {
-                    type: 'simplestore',
-                    autoLoad: true,
-                    model: 'App.model.tpm.discountrange.DiscountRange',
-                    extendedFilter: {
-                        xclass: 'App.ExtFilterContext',
-                        supportedModels: [{
-                            xclass: 'App.ExtSelectionFilterModel',
-                            model: 'App.model.tpm.discountrange.DiscountRange',
-                            modelId: 'efselectionmodel'
-                        }]
-                    }
-                }
-            }, {
-                xtype: 'combobox',
-                editable: false,
-                dataIndex: 'DurationRangeId',
                 displayField: 'Name',
                 entityType: 'DurationRange',
-                fieldLabel: l10n.ns('tpm', 'PlanPostPromoEffect').value('DurationRangeName'),
-                queryMode: 'local',
-                valueField: 'Id',
-                forceSelection: true,
                 allowBlank: false,
                 allowOnlyWhitespace: false,
                 store: {
                     type: 'simplestore',
-                    autoLoad: true,
-                    model: 'App.model.tpm.durationrange.DurationRange',
-                    extendedFilter: {
-                        xclass: 'App.ExtFilterContext',
-                        supportedModels: [{
-                            xclass: 'App.ExtSelectionFilterModel',
-                            model: 'App.model.tpm.durationrange.DurationRange',
-                            modelId: 'efselectionmodel'
-                        }]
-                    }
-                }
+                    autoLoad: false,
+                    model: 'App.model.tpm.durationrange.DurationRange'
+                },
+                extendedFilter: {
+                    xclass: 'App.ExtFilterContext',
+                    supportedModels: [{
+                        xclass: 'App.ExtSelectionFilterModel',
+                        model: 'App.model.tpm.durationrange.DurationRange',
+                        modelId: 'efselectionmodel'
+                    }]
+                },
+                mapping: [{
+                    from: 'Name',
+                    to: 'DurationRangeName'
+                }]                
             }, {
                 xtype: 'numberfield',
                 name: 'PlanPostPromoEffectW1',
