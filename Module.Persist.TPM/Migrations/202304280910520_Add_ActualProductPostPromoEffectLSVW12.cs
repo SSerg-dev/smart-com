53c8ead1858489fb1cbbb09354495c0c261e79ba
Риф Яруллин <rif.iarullin@smartcom.software>
2023-04-28 14:12:26 +0500

    (PPE) Add_ActualProductPostPromoEffectLSVW12

---
+++ Module.Persist.TPM/Migrations/202304280910520_Add_ActualProductPostPromoEffectLSVW12.cs
@@ -0,0 +1,23 @@
+﻿namespace Module.Persist.TPM.Migrations
+{
+    using Core.Settings;
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_ActualProductPostPromoEffectLSVW12 : DbMigration
+    {
+        public override void Up()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            AddColumn($"{defaultSchema}.PromoProduct", "ActualProductPostPromoEffectLSVW1", c => c.Double());
+            AddColumn($"{defaultSchema}.PromoProduct", "ActualProductPostPromoEffectLSVW2", c => c.Double());
+        }
+        
+        public override void Down()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            DropColumn($"{defaultSchema}.PromoProduct", "ActualProductPostPromoEffectLSVW2");
+            DropColumn($"{defaultSchema}.PromoProduct", "ActualProductPostPromoEffectLSVW1");
+        }
+    }
+}
