809f142c5d88ed5fa984d5e4994d19fd9dd90a53
Риф Яруллин <rif.iarullin@smartcom.software>
2023-05-24 12:26:33 +0500

    (Sprint20) empty mig

---
+++ Module.Persist.TPM/Migrations/202305240725265_EmptyM24052023.cs
@@ -0,0 +1,16 @@
+﻿namespace Module.Persist.TPM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmptyM24052023 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
