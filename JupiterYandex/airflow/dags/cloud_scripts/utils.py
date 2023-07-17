7865dd87087016add5981a2a02946c9aa3d40272
Илья Черноскутов <ilya.chernoskutov@smartcom.software>
2023-07-17 17:19:30 +0300

    fix exception

--- JupiterYandex/airflow/dags/cloud_scripts/utils.py
+++ JupiterYandex/airflow/dags/cloud_scripts/utils.py
@@ -1,15 +1,13 @@
-#from airflow.exceptions import AirflowException
-
 class ProjectPathHelper:
     PROJECT_NAME="${ProjectName}"
     SYSTEM_NAME="${Environment}"
 
     def getPath():
         #exception will raise only if deploy pipeline won't process this file
-        raise AirflowException("Failed to initialize project path")
+        raise Exception("Failed to initialize project path")
         return f"{ProjectPathHelper.PROJECT_NAME}/{ProjectPathHelper.SYSTEM_NAME}/"
     
     def getDagId():
         #exception will raise only if deploy pipeline won't process this file
-        raise AirflowException("Failed to initialize project path")
+        raise Exception("Failed to initialize project path")
         return f"{ProjectPathHelper.PROJECT_NAME}_{ProjectPathHelper.SYSTEM_NAME}_"
