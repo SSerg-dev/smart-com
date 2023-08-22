42217daa7db23f5db73116f7a4aa3557f2779f52
Риф Яруллин <rif.iarullin@smartcom.software>
2023-08-22 17:33:28 +0300

    Merged PR 1653: (TLC) add AP support admin
    (TLC) add AP support admin

---
+++ Module.Persist.TPM/Migrations/202308221353109_Add_AP_SA_RPA.cs
@@ -0,0 +1,31 @@
+﻿namespace Module.Persist.TPM.Migrations
+{
+    using Core.Settings;
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_AP_SA_RPA : DbMigration
+    {
+        public override void Up()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            SqlString = SqlString.Replace("DefaultSchemaSetting", defaultSchema);
+            Sql(SqlString);
+        }
+        
+        public override void Down()
+        {
+        }
+        private string SqlString = @"
+            DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='SupportAdministrator');
+
+            INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+            VALUES
+                (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetRPASettings' AND Resource = 'RPASettings')),
+				(NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetRPAs' AND Resource = 'RPASettings')),
+				(NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'SaveRPA' AND Resource = 'RPAs'))
+            GO
+        ";
+    }
+}
