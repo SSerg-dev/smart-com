6daf40ac58c6e3867eb6c79d536b223cf5786ae9
Риф Яруллин <rif.iarullin@smartcom.software>
2023-08-25 19:00:59 +0500

    (TLC) merge

---
+++ Module.Persist.TPM/Migrations/202308251351555_Empty25082023.cs
@@ -0,0 +1,17 @@
+﻿namespace Module.Persist.TPM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Empty25082023 : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
