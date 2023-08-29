369fb2d0e8810715b0564dd72f740394eb81dada
Риф Яруллин <rif.iarullin@smartcom.software>
2023-08-29 08:24:42 +0300

    Merged PR 1672: (RSRA) fix ROI Report Future Price
    (RSRA) fix ROI Report Future Price
    
    Related work items: #334539

--- Module.Persist.TPM/Migrations/202308081207443_add_SavedPromo.cs
+++ Module.Persist.TPM/Migrations/202308081207443_add_SavedPromo.cs
@@ -33,6 +33,7 @@
 
             Sql(ViewMigrations.UpdateClientDashboardRSViewString(defaultSchema));
             Sql(ViewMigrations.GetPromoRSViewString(defaultSchema));
+            Sql(ViewMigrations.UpdatePromoPriceIncreaseROIReportViewString(defaultSchema));
         }
         
         public override void Down()
