01c40a3db031cb764134026c27cac7920795fb1f
Роман Егоров <roman.egorov@smartcom.software>
2023-10-02 12:43:02 +0300

    Merged PR 1762: Добавление индекса уникальности для таблицы долей
    Внесены изменения в модель ClientTreeBrandTech.
    Создана миграция

--- Module.Persist.TPM/Model/TPM/ClientTreeBrandTech.cs
+++ Module.Persist.TPM/Model/TPM/ClientTreeBrandTech.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Data;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Module.Persist.TPM.Model.TPM
@@ -8,15 +9,20 @@ namespace Module.Persist.TPM.Model.TPM
     {
         public Guid Id { get; set; }
         public bool Disabled { get; set; }
+        [Index("ClientTreeBrandTech__IDX", 4, IsUnique = true)]
         public DateTimeOffset? DeletedDate { get; set; }
 
         public double Share { get; set; }
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(20)]
+        [Index("ClientTreeBrandTech__IDX", 3, IsUnique = true)]
         public string ParentClientTreeDemandCode { get; set; }
         public string CurrentBrandTechName { get; set; }
 
-
+        [Index("ClientTreeBrandTech__IDX", 1, IsUnique = true)]
         public int ClientTreeId { get; set; }
         public virtual ClientTree ClientTree { get; set; }
+        [Index("ClientTreeBrandTech__IDX",2 , IsUnique = true)]
         public Guid BrandTechId { get; set; }
         public virtual BrandTech BrandTech { get; set; }
 
