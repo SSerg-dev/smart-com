8eac0f2b200c9ac484e9819725364f3ba891aa8b
Роман Егоров <roman.egorov@smartcom.software>
2023-10-25 11:08:41 +0300

    Merged PR 1836: Add_migration_AccessPoint
    Добавил недостающие AccessPoints
    
    Related work items: #337507

---
+++ Module.Persist.TPM/Migrations/202310241354004_Add_sfa_accesPoint.cs
@@ -0,0 +1,109 @@
+﻿namespace Module.Persist.TPM.Migrations
+{
+    using Core.Settings;
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class Add_sfa_accesPoint : DbMigration
+    {
+        public override void Up()
+        {
+            var defaultSchema = AppSettingsManager.GetSetting<string>("DefaultSchema", "dbo");
+            SqlString = SqlString.Replace("DefaultSchemaSetting", defaultSchema);
+            Sql(SqlString);
+        }
+
+        public override void Down()
+        {
+
+        }
+        private string SqlString = @" DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='Administrator');
+
+                INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+                VALUES
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetDeletedSFAType' AND Resource = 'DeletedSFATypes')),                                       
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetDeletedSFATypes' AND Resource = 'DeletedSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetHistoricalSFATypes' AND Resource = 'HistoricalSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'DeletedSFATypes')),                  
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'HistoricalSFATypes')) 
+                GO
+
+                DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='SupportAdministrator');
+
+                INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+                VALUES
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetDeletedSFAType' AND Resource = 'DeletedSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetDeletedSFATypes' AND Resource = 'DeletedSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetHistoricalSFATypes' AND Resource = 'HistoricalSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'DeletedSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'HistoricalSFATypes'))
+                GO
+
+                DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='FunctionalExpert');
+
+                INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+                VALUES
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetDeletedSFAType' AND Resource = 'DeletedSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetDeletedSFATypes' AND Resource = 'DeletedSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetHistoricalSFATypes' AND Resource = 'HistoricalSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'DeletedSFATypes')),
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'HistoricalSFATypes'))
+                GO
+
+                DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='DemandPlanning');
+
+                INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+                VALUES
+                      (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetHistoricalSFATypes' AND Resource = 'HistoricalSFATypes')) ,
+                      (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'HistoricalSFATypes'))
+                GO
+
+                DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='KeyAccountManager');
+
+                INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+                VALUES
+                         (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetHistoricalSFATypes' AND Resource = 'HistoricalSFATypes')) ,
+                      (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'HistoricalSFATypes'))                    
+                GO
+
+                DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='CustomerMarketing');
+
+                INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+                VALUES
+                     (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetHistoricalSFATypes' AND Resource = 'HistoricalSFATypes')) ,
+                     (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'HistoricalSFATypes'))                    
+                GO
+
+                DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='DemandFinance');
+
+                INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+                VALUES
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetHistoricalSFATypes' AND Resource = 'HistoricalSFATypes')) ,
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'HistoricalSFATypes'))                    
+                GO
+
+                DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='GAManager');
+
+                INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+                VALUES
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetHistoricalSFATypes' AND Resource = 'HistoricalSFATypes')) ,
+                    (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'HistoricalSFATypes'))                    
+                GO
+                DECLARE @RoleId uniqueidentifier = (SELECT [Id] FROM [DefaultSchemaSetting].[Role] where SystemName='SuperReader');
+
+                INSERT INTO [DefaultSchemaSetting].[AccessPointRole]
+                ([Id],[RoleId],[AccessPointId])
+                VALUES
+                     (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetHistoricalSFATypes' AND Resource = 'HistoricalSFATypes')) ,
+                     (NEWID(), @RoleId, (SELECT TOP(1) Id FROM [DefaultSchemaSetting].[AccessPoint] WHERE Action = 'GetFilteredData' AND Resource = 'HistoricalSFATypes'))                  
+                GO";
+    }
+}
