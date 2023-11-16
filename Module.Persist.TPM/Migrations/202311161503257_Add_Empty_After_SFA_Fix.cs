a328e18707c81797a1f0e7e5cf2702800a39d32b
Илья Черноскутов <ilya.chernoskutov@smartcom.software>
2023-11-16 07:07:24 -0800

    Empty migration

---
+++ Module.Persist.TPM/Migrations/202311161503257_Add_Empty_After_SFA_Fix.cs
@@ -0,0 +1,16 @@
+﻿namespace Module.Persist.TPM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_Empty_After_SFA_Fix : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
